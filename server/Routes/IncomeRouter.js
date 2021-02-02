5a3ecf072fb96be635e7be1b3bf1643175b18819
Dominik Tyc <domikxf@gmail.com>
2021-02-02 19:33:14 +0100

    Change account balance

--- server/Routes/IncomeRouter.js
+++ server/Routes/IncomeRouter.js
@@ -1,15 +1,25 @@
 const express = require("express");
-const { Income } = require("../sequelize");
+const { Sequelize } = require("sequelize");
+const { Income, Account } = require("../sequelize");
 
 const router = express.Router();
 
 router.post("/", async (req, res) => {
-  Income.create({
+  await Income.create({
     name: req.body.name,
     date: req.body.date,
     value: req.body.value,
     AccountId: req.body.accountId,
   }).then((x) => res.json(x));
+
+  await Account.update(
+    { balance: Sequelize.literal(`balance + ${req.body.value}`) },
+    {
+      where: {
+        id: req.body.accountId,
+      },
+    }
+  );
 });
 
 module.exports = router;
