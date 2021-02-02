783fe8b4d03a636e525dad3f6a0270650a5c6b48
Dominik Tyc <domikxf@gmail.com>
2021-02-02 12:36:18 +0100

    Init api

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+
+const PORT = 5000;
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "OK",
+  });
+});
+
+app.listen(PORT, () => {
+    console.log(`API is running on port: ${PORT}`);
+});
