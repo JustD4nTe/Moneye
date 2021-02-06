769e1c095e91dae0c4b1f7e62cf5ad3c928d5f98
san <upsu90@gmail.com>
2021-02-06 18:52:35 +0100

    Install react hook form and yup

--- client/src/components/Spending/AddSpending.jsx
+++ client/src/components/Spending/AddSpending.jsx
@@ -2,23 +2,19 @@ import React from "react";
 import "../../App.css";
 import "../../index.css";
 import { makeStyles } from "@material-ui/core/styles";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 export default function AddSpending() {
-    const style = makeStyles({
+  const style = makeStyles({})();
 
-    })();
-  
-    return (
-      <div className="widget">
-        <h2>Add spending</h2>
-  
-        {/* Add spending form*/}
-        <div className={style.form}>
+  return (
+    <div className="widget">
+      <h2>Add spending</h2>
 
-        </div>
-  
-
-      </div>
-    );
-  }
-  
+      {/* Add spending form*/}
+      <div className={style.form}></div>
+    </div>
+  );
+}
