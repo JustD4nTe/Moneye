8f86ab57663053e8d43734942202554be48328ad
san <upsu90@gmail.com>
2021-02-06 18:24:55 +0100

    Create basic AddSpending component

---
+++ client/src/components/Spending/AddSpending.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "../../App.css";
+import "../../index.css";
+import { makeStyles } from "@material-ui/core/styles";
+
+export default function AddSpending() {
+    const style = makeStyles({
+
+    })();
+  
+    return (
+      <div className="widget">
+        <h2>Add spending</h2>
+  
+        {/* Add spending form*/}
+        <div className={style.form}>
+
+        </div>
+  
+
+      </div>
+    );
+  }
+  
--- client/src/pages/spendings.js
+++ client/src/pages/spendings.js
@@ -2,6 +2,7 @@ import React from "react";
 import "../App.css";
 import { makeStyles } from "@material-ui/core/styles";
 import LastSpending from "../components/Spending/LastSpending";
+import AddSpending from "../components/Spending/AddSpending";
 
 export default function Spendings() {
   const style = makeStyles({
@@ -14,7 +15,7 @@ export default function Spendings() {
     <div>
       <h1>Spendings</h1>
       <div className={`${style.widgets} widgets-container`}>
-        <LastSpending />
+        <AddSpending />
         <LastSpending />      
         <LastSpending />
       </div>
